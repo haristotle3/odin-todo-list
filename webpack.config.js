aae9f74653f10a5ef0a446f3a0b867e8604b1231
haristotle <haristotlepatil2003@gmail.com>
2025-06-24 19:09:59 +0530

    Init webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,41 @@
-const path = require('path')
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  devtool: "eval-source-map",
+  devServer: {
+    watchfiles: ["./src/template.html"],
+  },
+};
