346a0ab7b54369fe79b2311907ee4d2b18489395
haristotle <haristotlepatil2003@gmail.com>
2025-06-26 01:37:20 +0530

    Implement todoItem class

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,4 +1,59 @@
+import { format, compareAsc } from "date-fns";
 class TodoItem {
-  constructor(title) {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = ``;
+    this.complete = false;
+  }
+
+  toggleComplete() {
+    this.complete = !this.complete;
+  }
+
+  updateTitle(newTitle) {
+    this.title = newTitle;
+  }
+
+  updateDescription(newDescription) {
+    this.description = newDescription;
+  }
+
+  updateDueDate(newDueDate) {
+    this.dueDate = newDueDate;
+  }
+
+  updatePriority(newPriority) {
+    this.priority = newPriority;
+  }
+
+  updateNotes(newNotes) {
+    this.notes = newNotes;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+
+  getPriority() {
+    return this.priority;
+  }
+
+  getNotes() {
+    return this.notes;
+  }
+
+  isComplete() {
+    return this.complete;
   }
 }
